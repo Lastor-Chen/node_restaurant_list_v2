63183d4c05344821213aecbefb3fbcd90c40ab2b
Lastor <lasotraya@gmail.com>
2019-09-07 22:28:41 +0800

    重整 app.js code排版

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
-// ////////////////
-// 環境設定
+// App Server
 
-// 引入 framework
-const express = require('express')
+// import
+// ==============================
+
+// 引入 npm package
+const express = require('express')                      // framework
+const mongoose = require('mongoose')                    // mongoDB ODM
+const exphbs = require('express-handlebars')            // template engine
+const methodOverride = require('method-override')       // middleware
+
+// 引入 Model
+const Restaurant = require('./models/restaurant.js')    // mongoose Model
+
+
+// 環境 setup
+// ==============================
+
 const app = express()
 
-// 引入資料庫 ODM
-const mongoose = require('mongoose')
+// 連接 mongoDB
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'mongoDB connection error.'))
 db.once('open', console.log.bind(console, 'mongoDB is connected.'))
 
-// 引入 mongoose model
-const Restaurant = require('./models/restaurant.js')
-
-// 設置 template engine
-const exphbs = require('express-handlebars')
-const option = {
-  extname: 'hbs',
-  defaultLayout: 'main',
-}
-
+// 設定 template engine
+const option = { extname: 'hbs', defaultLayout: 'main' }
+  
 app.engine('hbs', exphbs(option) )
 app.set('view engine', 'hbs')
 
-// 引入 method-override
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
-
 // Server 相關 setting
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
 
+app.use(express.static('public'))
 
-// ////////////////
-// 路由設定
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+
+// route 設定
+// ==============================
 
 app.use('/', require('./routers/index.js'))
 app.use('/search', require('./routers/search.js'))
 app.use('/restaurants', require('./routers/restaurants.js'))
 
-// ////////////////
-// 啟動 Server
+
+// Start Server
+// ==============================
 app.listen(app.get('port'), () => {
   console.log(`Node.js Server with Express is running => http://localhost:${app.get('port')}`)
 })
