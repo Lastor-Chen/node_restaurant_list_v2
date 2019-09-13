5df4e92f56d2ed725a23d35dd8b9830ec0de9f06
Lastor <lasotraya@gmail.com>
2019-09-14 01:01:51 +0800

    加入 bcrypt 功能，將 password 暗號化

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,7 @@
 
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user.js')
+const bcrypt = require('bcryptjs')
 
 // 設定 Local Strategy
 // ==============================
@@ -17,10 +18,17 @@ function localCallback (email, password, done) {
   User.findOne({ email }, (err, user) => {
     if (err) return console.error(err)
 
+    // 檢查 email 帳號
     if (!user) return done(null, false, { message: '查無此帳號，請重新輸入' })
-    if (password !== user.password) return done(null, false, { message: '密碼不符，請重新輸入' })
     
-    done(null, user)
+    // 檢查加鹽後的密碼
+    bcrypt.compare(password, user.password, (err, success) => {
+      if (err) return console.error(err)
+
+      if (!success) return done(null, false, { message: '密碼不符，請重新輸入' })
+      
+      done(null, user)
+    })
   })
 }
 
--- routers/users.js
+++ routers/users.js
@@ -7,6 +7,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user.js')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // route '/users'
 // =========================
@@ -38,9 +39,14 @@ router.post('/signup', (req, res) => {
     // 如果 email 已註冊
     if (user) return res.send('email已被註冊')
     
-    // 確認 email 未註冊過，儲存帳戶到資料庫
-    User.create(input, (err) => console.error(err))
-    res.redirect('/index')
+    // 密碼加鹽雜湊化，儲存帳戶到資料庫
+    bcrypt.hash(input.password, 10, (err, hash) => {
+      if (err) return console.error(err)
+      
+      input.password = hash
+      User.create(input, (err) => console.error(err))
+      res.redirect('/index')
+    })
   })
 })
 
