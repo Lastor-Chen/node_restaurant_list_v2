a2425f578c4948f643414ac277d6d792fe3ecc45
Lastor <lasotraya@gmail.com>
2019-09-14 21:57:26 +0800

    調整 UI，餐廳、登入、註冊表單

--- routers/users.js
+++ routers/users.js
@@ -14,7 +14,7 @@ const bcrypt = require('bcryptjs')
 
 // 登入
 router.get('/signin', (req, res) => { 
-  res.render('signin', { js: ['popUp'] })
+  res.render('signin', { js: ['popUp'], css: 'sign', signin: 'signin' })
 })
 
 router.post('/signin', 
@@ -28,23 +28,25 @@ router.post('/signin',
 
 // 註冊
 router.get('/signup', (req, res) => {
-  res.render('signup', { js: ['popUp'] })
+  res.render('signup', { js: ['popUp'], css: 'sign' })
 })
 
 router.post('/signup', (req, res, next) => {
-  const input = { ...req.body }
+  // 深拷貝，保護原資料讓 passport 能抓取原始密碼
+  const input = { ...req.body }  
 
   let error = ''
   if (!input.email || !input.password) { error = 'Email 與 Password 不得為空' }
   if (input.password !== input.password2) { error = '密碼不一致' }
 
-  if (error.length) return res.render('signup', { input, error })
+  if (error.length) return res.render('signup', { input, error, css: 'sign' })
 
   User.findOne({ email: input.email }, (err, user) => {
     if (err) console.error(err)
 
     // 如果 email 已註冊
-    if (user) return res.render('signup', { input, error: '此 Email 已被使用' })
+    error = '此 Email 已被使用'
+    if (user) return res.render('signup', { input, error, css: 'sign' })
     
     // 密碼加鹽雜湊化，儲存帳戶到資料庫
     bcrypt.hash(input.password, 10, async (err, hash) => {
