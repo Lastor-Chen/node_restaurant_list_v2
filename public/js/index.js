f6a863c4dfb9fea0057b798b93892fe45da947ca
Lastor <lasotraya@gmail.com>
2019-09-16 16:44:44 +0800

    修正 外部網站連入時會自動關閉視窗的bug

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,16 @@
-// 確認當前窗口是否為 OAuth pop-up
-if (window.opener) {
+/*
+ * 說明：
+ * 此 app 使用 pop-up 處理 OAuth 授權。
+ * 如果是此 app 開出的 pop-up 窗口，將帶有 window.name = "OAuth" 屬性。
+ * 授權確認後，會在 pop-up 導回首頁，而非期望的原視窗。
+ * 所以透過判斷 window.name = "OAuth" 來關閉 pop-up，並將原視窗導進首頁。
+*/
+
+// 確認當前窗口是否為此 app 開啟的 OAuth pop-up
+if (window.name === 'OAuth') {
   // 父視窗重新導向
   window.opener.location.replace('/index')
 
-  // 關閉子視窗
+  // 關閉 pop-up 視窗
   window.close()
 }
--- public/js/popUp.js
+++ public/js/popUp.js
@@ -1,5 +1,13 @@
-// 確認當前窗口是否為 OAuth pop-up
-if (window.opener) {
+/*
+ * 說明： 
+ * 於登入/註冊頁，點擊 FB OAuth 按鈕時，開出彈出窗口並給予 window.name = "OAuth"
+ * 如果 User 取消操作，pop-up 會回到 /signin 頁面。
+ * 此時可以透過 window.name 這個 flag 將 pop-up 關閉。
+ * 如果是從外部網站進入此 app，將不會具有這個 flag，用來避免 window.close() 誤觸發。
+*/
+
+// 確認當前窗口是否為此 app 開啟的 OAuth pop-up
+if (window.name === 'OAuth') {
   window.close()
 }
 
@@ -13,6 +21,7 @@ $('#fb-btn').on('click', e => {
   // 設定 pop-up 視窗位置
   const left = (screen.width / 2) - (width / 2)
   const top = (screen.height / 2) - (height / 1.7)
+
 
   window.open('/OAuth/facebook', 'OAuth', `width=${width}, height=${height}, left=${left}, top=${top}`)
 })
